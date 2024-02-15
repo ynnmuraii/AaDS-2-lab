f8d31596872b4caddf83d51ac717abac1db7c1fc
ynnmuraii <tihookeanski.y.tanruuu@gmail.com>
2024-02-16 00:02:04 +0400

    added to main some tests (cuz google.test didn`t work by pathway)

--- console/console_app.cc
+++ console/console_app.cc
@@ -25,3 +25,51 @@ int main() {
 
     return 0;
 }
+
+//LinkedList<int> randomList(5);
+//cout << randomList << "\nRandom constructor works.\n" << endl;
+
+
+//LinkedList<int> assignedList;
+//assignedList = studentsList;
+//cout << assignedList << "\nAssignment operator works.\n" << endl;
+
+//
+//studentsList.push_tail(new Node<int>("Ivanov", "Ivan", 1, 4.5));
+//cout << studentsList << "\nPush_tail works.\n" << endl;
+
+
+//studentsList.push_tail(randomList);
+//cout << studentsList << "\nPush_tail(overloaded) works.\n" << endl;
+
+
+//studentsList.push_head(new Node<int>("Danila", "Ivanovich", 2, 5));
+//cout << studentsList << "\nPush_head works.\n" << endl;
+
+
+//studentsList.push_head(studentsList);
+//cout << studentsList << "\nPush_head (overloaded) works.\n" << endl;
+
+
+//studentsList.pop_head();
+//cout << studentsList << "\nPop_head works.\n" << endl;
+
+
+//studentsList.pop_tail();
+//cout << studentsList << "\nPop_tail works.\n" << endl;
+
+
+//studentsList.delete_node("Ivanov", "Ivan", 1); // Удалим ученика с указанными данными
+//cout << studentsList << "\nDelete_node works.\n" << endl;
+
+
+//Node<int>* firstStudent = studentsList[0];
+//if (firstStudent != nullptr) {
+//    cout << "\nFirst student: " << firstStudent->lastName << "\n" << endl;
+//} else {
+//    cout << "\nList is empty." << "\n" << endl;
+//}
+
+
+//studentsList[0]->averageScore = 4.8; 
+//cout << "\nAverage score of the first student changed to: " << studentsList[0]->averageScore << endl;
